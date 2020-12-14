c0e501de24dcb239cc686e668d3a5bf5577b1a4c
danielkwon89 <danielkwon89@gmail.com>
2020-12-13 21:08:23 -0500

    Migrations migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_14_004330) do
+ActiveRecord::Schema.define(version: 2020_12_14_014038) do
 
   create_table "players", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "scores", force: :cascade do |t|
+    t.integer "score_value"
+    t.integer "player_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
