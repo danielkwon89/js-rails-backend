13f36e7f535cbc40033de5d2db0f70491af6af6b
danielkwon89 <danielkwon89@gmail.com>
2020-12-14 10:44:52 -0500

    Created create route for scores_controller

--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -5,4 +5,19 @@ class Api::V1::ScoresController < ApplicationController
         render json: scores
     end
 
+    def create
+        score = Score.new(score_params)
+
+        if score.save
+            render json: score, status: :accepted
+        else
+            render json: {errors: score.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def syllabus_params
+        params.require(:syllabus).permit(:score_value, :player_id)
+    end
 end
